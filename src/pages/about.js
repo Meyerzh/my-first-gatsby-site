701392ba66e0a9bd803aa1963249f14c2e6fec5f
Meyerz <meyerz@ciandt.com>
2022-04-21 12:41:00 +0800

    Create a new page component for an About page.

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About me</title>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, white I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
